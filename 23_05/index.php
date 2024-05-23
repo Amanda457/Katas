d83a79249bc41236dd1d5e312a03cb3827edaa36
Amanda457 <mandy96lomi@gmail.com>
2024-05-23 11:31:55 +0200

    done

---
+++ 23_05/index.php
@@ -0,0 +1,41 @@
+<?php
+
+function calcularQuadrat(int $num): string{
+
+    $resultat = $num * $num;
+    $missatgeQ = "Quadrat: " . $resultat . PHP_EOL;
+    return $missatgeQ;
+}
+
+function calcularCub(int $num): string{
+
+    $resultat = $num * $num * $num;
+    $missatgeC = "Cub: " . $resultat . PHP_EOL;
+    return $missatgeC;
+}
+
+function calcularFactorial(int $num): string{
+    $resultat = $num;
+    for ($i = 1; $i < $num; $i++) {
+        $resultat = $resultat * $i;
+    }
+
+    $missatgeF = "Factorial: " . $resultat . PHP_EOL;
+    return $missatgeF;
+}
+
+function imprimirCalculs(int $num): string{
+    $quadrat = calcularQuadrat($num);
+    $cub = calcularCub($num);
+    $factorial = calcularFactorial($num);
+
+    return $missatge = "Numero: " . $num . PHP_EOL . $quadrat . $cub . $factorial . PHP_EOL;
+}
+
+function openAndWriteCalc ($num){
+    $calculs = imprimirCalculs($num);
+    $fp = fopen("calculs_$num.txt", "w");
+    fwrite($fp, $calculs);
+}
+
+openAndWriteCalc(5);
