d532a4dcbf9a7d6c55b7cc9ebe1ffce12fe770fb
Amanda457 <mandy96lomi@gmail.com>
2024-07-11 10:22:50 +0200

    refactor magic matrix

--- 16_05/matriu_magica.php
+++ 16_05/matriu_magica.php
@@ -3,26 +3,7 @@
 $magicSquare = [2,7,6,9,5,1,4,3,8];
 $magicSquare2 = [3,7,6,9,5,1,4,3,8];
 
-/* OLD STYLE CON ARRAY SIMPLE
-function comprovarMagicSquare (array $array) : void {
-
-    if (($array[0]+$array[1]+ $array[2]) === ($array[3]+$array[4]+ $array[5]) && ($array[3]+$array[4]+ $array[5]) === ($array[6]+$array[7]+ $array[8])){
-        //echo "files ok".PHP_EOL;
-        if ( ($array[0]+$array[3]+ $array[6]) === ($array[1]+$array[4]+ $array[7]) && ($array[1]+$array[4]+ $array[7]) === ($array[2]+$array[5]+ $array[8])){
-           //echo "columnes ok".PHP_EOL;
-            if (  ($array[0]+$array[4]+ $array[8]) === ($array[2]+$array[4]+ $array[6])){
-                echo "És una matriu màgica!".PHP_EOL;
-            } else {
-                echo "No és una matriu màgica!".PHP_EOL;
-                }
-        }else {
-            echo "No és una matriu màgica!".PHP_EOL;}
-    }else {
-        echo "No és una matriu màgica!".PHP_EOL;
-    }
-}*/
-
-function sumarFiles (array $array) : array {
+/*function sumarFiles (array $array) : array {
     $fila1 = $array[0]+$array[1]+ $array[2];
     $fila2 = $array[3]+$array[4]+ $array[5];
     $fila3 = $array[6]+$array[7]+ $array[8];
@@ -30,15 +11,27 @@ function sumarFiles (array $array) : array {
     $valorFiles = [$fila1, $fila2, $fila3];
 
     return $valorFiles;
+}*/
+
+function sumarFiles(array $array): array {
+    $valorFiles = [];
+    for ($i = 0; $i < 3; $i++) {
+        $valorFiles[$i] = 0;
+        for ($j = 0; $j < 3; $j++) {
+            $valorFiles[$i] += $array[$i * 3 + $j];
+        }
+    }
+    return $valorFiles;
 }
 
-function sumarColumnes (array $array) : array {
-    $columna1 = $array[0]+$array[3]+ $array[6];
-    $columna2 = $array[1]+$array[4]+ $array[7];
-    $columna3 = $array[2]+$array[5]+ $array[8];
-
-    $valorColumnes = [$columna1, $columna2, $columna3];
-
+function sumarColumnes(array $array): array {
+    $valorColumnes = [];
+    for ($i = 0; $i < 3; $i++) {
+        $valorColumnes[$i] = 0;
+        for ($j = 0; $j < 3; $j++) {
+            $valorColumnes[$i] += $array[$j * 3 + $i];
+        }
+    }
     return $valorColumnes;
 }
 
@@ -46,9 +39,7 @@ function sumarDiagonals (array $array) : array {
     $diagonal1 = $array[0]+$array[3]+ $array[6];
     $diagonal2 = $array[1]+$array[4]+ $array[7];
  
-    $valorDiagonals = [$diagonal1, $diagonal2];
-
-    return $valorDiagonals;
+    return [$diagonal1, $diagonal2];
 }
 function comprovarMagicSquare (array $array) : void {
     $files = sumarFiles($array);
@@ -73,4 +64,5 @@ function comprovarMagicSquare (array $array) : void {
 
 comprovarMagicSquare($magicSquare);
 comprovarMagicSquare($magicSquare2);
+
 ?>
