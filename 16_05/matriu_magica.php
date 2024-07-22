ed58b076fc45ad6acf61010752b6668402b421ee
Amanda457 <mandy96lomi@gmail.com>
2024-07-22 07:48:07 +0200

    refactor capitulo xxvi

--- 16_05/matriu_magica.php
+++ 16_05/matriu_magica.php
@@ -1,19 +1,10 @@
-<?php 
+<?php
 
-$magicSquare = [2,7,6,9,5,1,4,3,8];
-$magicSquare2 = [3,7,6,9,5,1,4,3,8];
+$magicSquare = [2, 7, 6, 9, 5, 1, 4, 3, 8];
+$magicSquare2 = [3, 7, 6, 9, 5, 1, 4, 3, 8];
 
-/*function sumarFiles (array $array) : array {
-    $fila1 = $array[0]+$array[1]+ $array[2];
-    $fila2 = $array[3]+$array[4]+ $array[5];
-    $fila3 = $array[6]+$array[7]+ $array[8];
-
-    $valorFiles = [$fila1, $fila2, $fila3];
-
-    return $valorFiles;
-}*/
-
-function sumarFiles(array $array): array {
+function sumarFiles(array $array): array
+{
     $valorFiles = [];
     for ($i = 0; $i < 3; $i++) {
         $valorFiles[$i] = 0;
@@ -24,7 +15,8 @@ function sumarFiles(array $array): array {
     return $valorFiles;
 }
 
-function sumarColumnes(array $array): array {
+function sumarColumnes(array $array): array
+{
     $valorColumnes = [];
     for ($i = 0; $i < 3; $i++) {
         $valorColumnes[$i] = 0;
@@ -35,34 +27,26 @@ function sumarColumnes(array $array): array {
     return $valorColumnes;
 }
 
-function sumarDiagonals (array $array) : array {
-    $diagonal1 = $array[0]+$array[3]+ $array[6];
-    $diagonal2 = $array[1]+$array[4]+ $array[7];
- 
+function sumarDiagonals(array $array): array
+{
+    $diagonal1 = $array[0] + $array[3] + $array[6];
+    $diagonal2 = $array[1] + $array[4] + $array[7];
+
     return [$diagonal1, $diagonal2];
 }
-function comprovarMagicSquare (array $array) : void {
+function comprovarMagicSquare(array $array): void
+{
     $files = sumarFiles($array);
     $columnes = sumarColumnes($array);
     $diagonals = sumarDiagonals($array);
 
-    if ($files[0] === $files[1] && $files[1] === $files[2]){
-        //echo "files ok".PHP_EOL;
-        if ($columnes[0] === $columnes[1] && $columnes[1] === $columnes[2]){
-           //echo "columnes ok".PHP_EOL;
-            if ( $diagonals[0] === $diagonals [1]){
-                echo "És una matriu màgica!".PHP_EOL;
-            } else {
-                echo "No és una matriu màgica!".PHP_EOL;
-                }
-        }else {
-            echo "No és una matriu màgica!".PHP_EOL;}
-    }else {
-        echo "No és una matriu màgica!".PHP_EOL;
+    if ($files[0] === $files[1] && $files[1] === $files[2] && $columnes[0] === $columnes[1] && $columnes[1] === $columnes[2]
+        && $diagonals[0] === $diagonals[1]) {
+        echo "És una matriu màgica!" . PHP_EOL;
+    } else {
+        echo "No és una matriu màgica!" . PHP_EOL;
     }
 }
 
 comprovarMagicSquare($magicSquare);
 comprovarMagicSquare($magicSquare2);
-
-?>
